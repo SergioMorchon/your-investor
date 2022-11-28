0d4232d1aa7f283e517490499f76aed24ef7f5f0
Sergio Morchón Poveda <sergio.morchon@outlook.com>
2022-11-28 23:10:30 +0100

    Persist authorization

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { ResponsiveLayout } from "../../common/layout";
 import { useFetchProfile, useProfileState } from "../../profile-context";
+import { useSessionContext } from "../../session-context";
 import { Profile } from "./profile";
 
 export const Dashboard = () => {
 	useFetchProfile();
 	const profileState = useProfileState();
+	const { logout } = useSessionContext();
 
 	return (
 		<ResponsiveLayout>
@@ -12,6 +14,7 @@ export const Dashboard = () => {
 			{profileState.isLoading && "Loading..."}
 			{profileState.error}
 			{profileState.profile ? <Profile profile={profileState.profile} /> : null}
+			<button onClick={logout}>Desconectar</button>
 		</ResponsiveLayout>
 	);
 };
--- src/session-context.tsx
+++ src/session-context.tsx
@@ -1,24 +1,51 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import {
+	createContext,
+	useCallback,
+	useContext,
+	useEffect,
+	useMemo,
+	useState,
+} from "react";
 
 type Context = {
 	authorization: string | null;
 	authorize: (authorization: string) => void;
+	logout: () => void;
 };
 
 const SessionContext = createContext<Context | void>(undefined);
+
+const AUTHORIZATION = "authorization";
+
+const usePersistAuthorization = (authorization: string | null) =>
+	useEffect(() => {
+		if (authorization) {
+			localStorage.setItem(AUTHORIZATION, authorization);
+		} else {
+			localStorage.removeItem(AUTHORIZATION);
+		}
+	}, [authorization]);
 
 type Props = {
 	children: React.ReactNode;
 };
 
 export const SessionContextProvider = ({ children }: Props) => {
-	const [authorization, setAuthorization] = useState<string | null>(null);
+	const [authorization, setAuthorization] = useState<string | null>(
+		localStorage.getItem(AUTHORIZATION)
+	);
+
+	usePersistAuthorization(authorization);
+
+	const logout = useCallback(() => {
+		setAuthorization(null);
+	}, []);
 
 	return (
 		<SessionContext.Provider
 			value={useMemo(
-				() => ({ authorization, authorize: setAuthorization }),
-				[authorization]
+				() => ({ authorization, authorize: setAuthorization, logout }),
+				[authorization, logout]
 			)}
 		>
 			{children}
